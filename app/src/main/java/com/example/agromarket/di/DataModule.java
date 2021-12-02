f0a143df893532ce5c89c6b5bbad6b73576acf74
Johann Bogota <johann.bogota@mail.escuelaing.edu.co>
2021-12-02 02:56:27 -0500

    config room completed

--- app/src/main/java/com/example/agromarket/di/DataModule.java
+++ app/src/main/java/com/example/agromarket/di/DataModule.java
@@ -1,13 +1,19 @@
 package com.example.agromarket.di;
 
+import android.content.Context;
+
+import androidx.room.Room;
+
 import com.example.agromarket.analytics.AnalyticsAdapter;
 import com.example.agromarket.analytics.GoogleAnalytics;
+import com.example.agromarket.room.AppDatabase;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
+import dagger.hilt.android.qualifiers.ApplicationContext;
 import dagger.hilt.components.SingletonComponent;
 
 @Module
@@ -20,4 +26,18 @@ public class DataModule {
         return new GoogleAnalytics();
     }
 
+    @Provides
+    @Singleton
+    public AppDatabase appDatabase(@ApplicationContext Context context)
+    {
+        AppDatabase db = Room.databaseBuilder(context,
+                AppDatabase.class, "database-name").build();
+        return db;
+    }
+
+    /*@Provides
+    @Singleton
+    public UserDao provideUserDao(AppDatabase appDatabase){
+        return appDatabase.userDao();
+    }*/
 }
