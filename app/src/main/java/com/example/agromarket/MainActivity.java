da75d5c80becaa237ed78511aec474058222a7b9
Johan Guerrero <johan@192.168.0.6>
2021-12-09 23:00:42 -0500

    vista subir imagen

--- app/src/main/java/com/example/agromarket/MainActivity.java
+++ app/src/main/java/com/example/agromarket/MainActivity.java
@@ -1,12 +1,24 @@
 package com.example.agromarket;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.example.agromarket.analytics.AnalyticsAdapter;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -18,15 +30,24 @@ import com.example.agromarket.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.HashMap;
 
 import javax.inject.Inject;
+
+import retrofit2.http.Url;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    private static final int SELECT_FILE = 1;
+    private Button bntSearch;
 
     @Inject
     public AnalyticsAdapter analyticsAdapter;
@@ -52,6 +73,8 @@ public class MainActivity extends AppCompatActivity {
                 analyticsAdapter.report("test" , new HashMap<>());
             }
         });
+
+        bntSearch = (Button) findViewById(R.id.btn_search);
     }
 
     @Override
@@ -81,5 +104,60 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    public void onClick(View view) {
+        abrirGaleria(view);
+    }
+
+
+    public void abrirGaleria(View v){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(
+                Intent.createChooser(intent, "Seleccione una imagen"),
+                SELECT_FILE);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        Uri selectedImageUri = null;
+        Uri selectedImage;
+
+        String filePath = null;
+        switch (requestCode) {
+            case SELECT_FILE:
+                if (resultCode == Activity.RESULT_OK) {
+                    selectedImage = imageReturnedIntent.getData();
+                    String selectedPath=selectedImage.getPath();
+
+                    Log.d("a",selectedImage.getLastPathSegment ());
+                    if (requestCode == SELECT_FILE) {
+
+                        if (selectedPath != null) {
+                            InputStream imageStream = null;
+                            try {
+                                imageStream = getContentResolver().openInputStream(
+                                        selectedImage);
+
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
+                            }
+
+                            // Transformamos la URI de la imagen a inputStream y este a un Bitmap
+                            Bitmap bmp = BitmapFactory.decodeStream(imageStream);
+
+
+                            // Ponemos nuestro bitmap en un ImageView que tengamos en la vista
+                            ImageView image = (ImageView) findViewById(R.id.imageId);
+                            image.setImageBitmap(bmp);
+
+                        }
+                    }
+                }
+                break;
+        }
     }
 }
